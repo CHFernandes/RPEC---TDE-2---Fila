11d7f2b60d5418a8b77147bcc9f946a4c375aa63
CHFernandes <chfernandes01@hotmail.com>
2019-04-22 22:31:52 -0300

    Add files via upload

---
+++ RPEC - TDE 2/src/Fila.java
@@ -0,0 +1,140 @@
+public class Fila {
+    private int primeiro;
+    private int MAX;
+    private int ultimo;
+    private int dado[];
+    private int tamanho;
+
+
+    public Fila(int n){
+
+        this.MAX = n;
+        this.primeiro = -1;
+        this.ultimo = -1;
+        this.dado = new int[n];
+        this.tamanho = 0;
+
+    }
+
+    public void inserir(int inserido){
+        if (!verificacheio()){
+            if (this.tamanho != this.MAX && this.ultimo != this.MAX){
+                if (this.primeiro == -1){
+                    this.primeiro = this.primeiro + 1;
+                }
+                if (this.ultimo == this.MAX-1){
+                    this.ultimo = -1;
+                }
+                this.ultimo = this.ultimo +1;
+                this.dado[this.ultimo] = inserido;
+                this.tamanho = this.tamanho + 1;
+                System.out.println("Valor Inserido");
+            }else {
+                if (primeiro != 0 && tamanho != this.MAX){
+                    this.tamanho = this.tamanho + 1;
+                    this.ultimo = 0;
+                    this.dado[this.primeiro] = inserido;
+                    System.out.println("Valor Inserido");
+                }
+            }
+        }else {
+            System.out.println("Fila Cheia");
+        }
+    }
+    
+    public void Remover() {
+		if (verificavazio() == false) {
+		if (this.primeiro == (MAX - 1)) {
+			dado[primeiro] = 0;
+			this.primeiro = 0;
+		} else
+		dado[primeiro] = 0;
+		this.primeiro = primeiro + 1;
+		this.tamanho = tamanho - 1;
+		System.out.println("Item removido");
+		} else
+			System.out.println("Fila Vazia");
+	}
+
+    public boolean verificacheio(){
+        if(this.tamanho != this.MAX){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public boolean verificavazio(){
+        if (this.tamanho == 0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public void imprimir(){
+        int p = this.primeiro;
+        int u = this.ultimo + 1;
+        if (p != u){
+            for (int i = p; i != u; i++){
+                System.out.println(this.dado[i]);
+                if (i == this.tamanho){
+                    i = -1;
+                }
+            }
+        }else {
+            for (int o = p; o != this.MAX; o++){
+                System.out.println(this.dado[o]);
+                if (this.tamanho - 1 == o){
+                    for (int b = 0; b != u; b++) {
+                        System.out.println(this.dado[b]);
+                    }
+                }
+            }
+        }
+    }
+
+    public int infirst(){
+        return this.primeiro;
+    }
+
+    public int first(){
+        return data()[infirst()];
+    }
+
+    public int inlast(){
+        return this.ultimo;
+    }
+
+    public int last(){
+        return data()[inlast()];
+    }
+
+    public int size(){
+        return this.tamanho;
+    }
+    public int sizeMAX(){
+        return this.MAX;
+    }
+
+    public void pop(){
+        if (!verificavazio()){
+            System.out.println(this.dado[this.primeiro]);
+            this.dado[this.primeiro] = 0;
+            if (this.primeiro == MAX -1){
+                this.primeiro = 0;
+            }else {
+                this.primeiro = this.primeiro + 1;
+            }
+            this.tamanho = this.tamanho - 1;
+            System.out.println("Item removido");
+        }else {
+            System.out.println("Fila vazia");
+        }
+    }
+
+    public int[] data(){
+        return this.dado;
+    }
+
+}
---
+++ RPEC - TDE 2/src/Main.java
@@ -0,0 +1,154 @@
+public class Main {
+	public static void main(String[] args) {
+		Fila primeira = construir(3);
+
+		inserir(primeira, 5);
+
+		cheia(primeira);
+
+		vazio(primeira);
+
+		remove(primeira);
+
+		vazio(primeira);
+
+		inserir(primeira, 1);
+		inserir(primeira, 2);
+		inserir(primeira, 30);
+		inserir(primeira, 40);
+
+		System.out.println(primeiro(primeira));
+
+		System.out.println(ultimo(primeira));
+
+		imprimir(primeira);
+
+		System.out.println();
+
+		//ordenar(primeira);
+
+		Fila segunda = construir(5);
+
+		inserir(segunda, 5);
+		inserir(segunda, 6);
+		inserir(segunda, 7);
+		inserir(segunda, 8);
+		inserir(segunda, 9);
+		inserir(segunda, 10);
+
+		imprimir(segunda);
+
+		System.out.println();
+
+		//ordenar(segunda);
+
+		Fila terceira = construir(3);
+		inserir(terceira,1);
+		inserir(terceira,30);
+		inserir(terceira,50);
+
+		imprimir(terceira);
+		System.out.println();
+
+		Fila quarta = construir(4);
+		inserir(quarta,2);
+		inserir(quarta,4);
+		inserir(quarta,6);
+		inserir(quarta,80);
+		System.out.println();
+
+		Fila quinta = merge(terceira, quarta);
+
+		System.out.println();
+
+		imprimir(quinta);
+
+	}
+
+	public static Fila merge(Fila a, Fila b) {
+		int na = tamanho(a);
+		int nb = tamanho(b);
+		//Fila temp = ordenar(a);
+		//Fila temp2 = ordenar(b);
+		int nc = na + nb;
+
+		Fila c = new Fila(nc);
+		int i = 0;
+		int j = 0;
+		while (i < na && j < nb) {
+			if (dado(a)[i] < dado(b)[j]) {
+				inserir(c, dado(a)[i++]);
+			} else {
+				inserir(c, dado(b)[j++]);
+			}
+		}
+		while (i < na) {
+			inserir(c, dado(a)[i++]);
+		}
+		while (j < nb) {
+			inserir(c, dado(b)[j++]);
+		}
+
+		return c;
+	}
+
+	public static void imprimir(Fila f) {
+		f.imprimir();
+	}
+
+	public static Fila construir(int limite) {
+		return new Fila(limite);
+	}
+
+	public static void inserir(Fila f, int valor) {
+		f.inserir(valor);
+	}
+
+	public static void cheia(Fila f) {
+		if (f.verificacheio()) {
+			System.out.println("Fila não está cheia");
+		} else {
+			System.out.println("Fila cheia");
+		}
+	}
+
+	public static void vazio(Fila f) {
+		if (f.verificavazio()) {
+			System.out.println("Fila vazia");
+		} else {
+			System.out.println("Fila não está vazia");
+		}
+	}
+
+	public static void remove(Fila f) {
+		f.pop();
+	}
+
+	public static int primeiro(Fila f) {
+		return f.first();
+	}
+
+	public static int ultimo(Fila f) {
+		return f.last();
+	}
+
+	public static int tamanho(Fila f) {
+		return f.size();
+	}
+
+	public static int[] dado(Fila f) {
+		return f.data();
+	}
+
+	/*public static Fila ordenar(Fila f) {
+		int p = f.infirst();
+		int u = f.inlast();
+		int i = p;
+		int j = u;
+		while(i  u) {
+			
+		}
+		return f;
+	}
+	*/
+}
